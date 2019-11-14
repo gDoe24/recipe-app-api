b467ef6c9634472c71e0abe7f1074453ad4ff0e0
gdoe24 <gregkeel11@gmail.com>
2019-11-14 14:34:02 -0500

    Added Ingredient Model

--- app/core/tests/tests_model.py
+++ app/core/tests/tests_model.py
@@ -47,3 +47,12 @@ class ModelTests(TestCase):
 			)
 
 		self.assertEqual(str(tag), tag.name)
+
+	def test_ingredients_str(self):
+		#Test string representation for ingredients 
+		ingredient=models.Ingredient.objects.create(
+			user=sample_user(),
+			name='Cucumber'
+			)
+		
+		self.assertEqual(str(ingredient),ingredient.name)
