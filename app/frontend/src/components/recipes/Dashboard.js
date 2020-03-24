3472f21b300a5f8752f804ff72bede5bc0049bb8
gdoe24 <gregkeel11@gmail.com>
2020-03-24 09:38:09 -0400

    Switched Form and myRecipes, to make better interaction between django frontend templates

--- app/frontend/src/components/recipes/Dashboard.js
+++ app/frontend/src/components/recipes/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import Form from "./Form";
+import MyRecipes from "./MyRecipes";
 import Header from "../layout/Header";
 
 export default function Dashboard() {
@@ -7,7 +7,7 @@ export default function Dashboard() {
     <Fragment>
     <Header />
     <div className="container">
-      <Form />
+      <MyRecipes/>
       </div>
     </Fragment>
   );
--- app/user/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from core.models import Ingredient
-
-class AddIngredientForm(forms.ModelForm):
-	
-	class Meta:
-		model = Ingredient
-		fields = ('name','amount','unit')
