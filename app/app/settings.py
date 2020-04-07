045fc8a3945ffc0bfb358e6ff873b8ca6b6fdb96
gdoe24 <gregkeel11@gmail.com>
2020-04-07 14:54:37 -0400

    Working with nginx to locate static files

--- app/app/settings.py
+++ app/app/settings.py
@@ -150,7 +150,7 @@ JWT_AUTH = {
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'frontend/static/')
+    os.path.join(BASE_DIR, 'core/static/')
 ]
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
